89ab41006f611094b0451e3a657408f534af0d14
Egor <egorsarantsev4565@yandex.ru>
2022-10-20 18:36:24 +0300

    0. Виды ссылок [#6854] - 2

--- src/main/java/ru/job4j/gc/ref/WeakDemo.java
+++ src/main/java/ru/job4j/gc/ref/WeakDemo.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class WeakDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        //example1();
+        example1();
         example2();
     }
 
